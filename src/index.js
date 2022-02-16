f8ecd973554e7eccd18b64221e1220cdb37c38c5
ChristopheBouriel <christophe.bouriel@gmail.com>
2022-02-16 18:01:37 +0100

    Loaders : mise en place de la gestion du style et de sass

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import $ from 'jquery'
+import 'bootstrap'
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// Si on veut simplement utiliser le css
+import './styles/styles.scss'
 
 $('#title').html('hello from Webpack');
-
--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,25 @@ module.exports = {
         filename: "js/site.js",
         path: path.resolve(__dirname, 'dist')
     },
+
     /*mode: 'development',
     watch: true,
     watchOptions: {
         ignored: /node_modules/
     }*/
     //inutile quand on a écrit les scripts dans le package.json
+
+    module: {
+        rules: [
+            {
+                test: /\.(css|scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'] // il y a un ordre : on utilise d'abord sass, puis css et enfin style
+            }
+        ]
+    }
+    /** Webpack ne connait que le JS, donc s'il doit traiter un autre type de fichier
+     *  il faut utiliser le loader correspondant et lui indiquer :
+     *  "Si tu vois une extension .css, alors utilise le css-loader et le style-loader"
+     *  La liste des loaders est disponible sur le site
+     */
 }
