0f41c02626333213231cfb23810ca241708d7cb2
ChristopheBouriel <christophe.bouriel@gmail.com>
2022-02-16 17:44:41 +0100

    Mise en place initiale

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import $ from 'jquery'
+
+$('#title').html('hello from Webpack');
+
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "js/site.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    /*mode: 'development',
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/
+    }*/
+    //inutile quand on a écrit les scripts dans le package.json
+}
